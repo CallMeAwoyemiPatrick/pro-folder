dcf2a9a305e6531562386e14a9fcb5d0b469a8a4
Awoyemi-Patrick <94967207+CallMeAwoyemiPatrick@users.noreply.github.com>
2022-01-02 01:55:47 -0800

    Real Estate Project

---
+++ pro.js
@@ -0,0 +1,25 @@
+// discription section
+const items = document.querySelectorAll('.item');
+items.forEach(item=>{
+  item.addEventListener('click',()=>{
+    items.forEach(item=> item.classList.remove('show'))
+    item.classList.add('show')
+  })
+})
+
+
+// form element 
+
+
+
+  // get the form elements defined in your form HTML above
+
+
+  // Success and Error functions for after the form is submitted
+
+ 
+
+  // handle the form submission event
+
+
+// End of form element */
